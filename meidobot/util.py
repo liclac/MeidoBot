57c2a373ad8c77101dcc784b3b097885fb83b9dc
Johannes Ekberg <uppfinnarn@gmail.com>
2013-03-14 15:44:59 +0100

    I may just have invented the neatest periodic timer ever - @ticking

--- meidobot/util.py
+++ meidobot/util.py
@@ -1,6 +1,17 @@
 import threading
+from time import sleep
 
-class MeidoWorker(threading.Thread, name = None, interval = 60, function = None):
-	def __init__(self, **kwargs):
-		threading.Thread(self, kwargs)
-		
+class ticking(object):
+	interval = None
+	
+	def __init__(self, interval):
+		self.interval = interval
+	def __call__(self, f):
+		def wrapper():
+			while True:
+				print "Wrapper!"
+				sleep(self.interval)
+		t = threading.Thread(target = wrapper)
+		t.daemon = True
+		t.start()
+		return f
