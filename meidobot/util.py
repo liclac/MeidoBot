c3bf86a072f0578bec3c2fbb5602a5161b4f0b5c
Johannes Ekberg <uppfinnarn@gmail.com>
2013-03-15 10:38:19 +0100

    Fixed @ticking, added @background and @async

--- meidobot/util.py
+++ meidobot/util.py
@@ -1,17 +1,34 @@
 import threading
+from functools import wraps
 from time import sleep
 
+def async(f):
+	@wraps(f)
+	def wrapper(*args, **kwargs):
+		t = threading.Thread(target = f)
+		t.start()
+	return wrapper
+
+def background(f):
+	def wrapper():
+		t = threading.Thread(target = f)
+		t.daemon = True
+		t.start()
+		return t
+	return wrapper
+
+# We need a class for this, since it takes parameters
+# I don't think it's possible to make function decorators
+# take parameters, but I've been wrong before...
 class ticking(object):
 	interval = None
 	
 	def __init__(self, interval):
 		self.interval = interval
 	def __call__(self, f):
+		@background
 		def wrapper():
 			while True:
-				print "Wrapper!"
+				f()
 				sleep(self.interval)
-		t = threading.Thread(target = wrapper)
-		t.daemon = True
-		t.start()
 		return f
