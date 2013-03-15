44c226d37650a8d15b701d5b808009b36c1c4930
Johannes Ekberg <uppfinnarn@gmail.com>
2013-03-15 11:44:03 +0100

    Yeeah concurrency

--- meidobot/util.py
+++ meidobot/util.py
@@ -2,40 +2,44 @@ import threading
 from functools import wraps
 from time import sleep
 
+def _f_thread(f, *args, **kwargs):
+	def run():
+		f(*args, **kwargs)
+	t = threading.Thread(target=run)
+	return t
+
 def async(f):
 	'''Makes calls to a function asynchronous.
 	Asynchronous functions cannot return a value.'''
 	@wraps(f)
 	def wrapper(*args, **kwargs):
-		def run():
-			f(*args, **kwargs)
-		t = threading.Thread(target = run)
+		t = _f_thread(f, *args, **kwargs)
 		t.start()
 		return t
 	
 	return wrapper
 
-def background(f):
-	'''Makes a function asynchronous and starts it immediately when it's loaded'.'''
+def daemon(f):
+	'''Makes calls to a function asynchronous on a daemon thread.
+	The daemon property means it's possible to cancel execution at
+	any time, so don't use this for things that have to finish!
+	Asynchronous functions cannot return a value.'''
 	@wraps(f)
 	def wrapper(*args, **kwargs):
-		def run():
-			f(*args, **kwargs)
-		t = threading.Thread(target = run)
+		t = _f_thread(f, *args, **kwargs)
 		t.daemon = True
 		t.start()
 		return t
-	
-	wrapper()
 	return wrapper
 
-# We need a class for this, since it takes parameters
-# I don't think it's possible to make function decorators
+# We need a class for this, since it takes parameters.
+# I don't think it's possible to make decorator functions
 # take parameters, but I've been wrong before...
 class ticking(object):
 	'''Makes a function fire once every n seconds, starting from
-	when the function is first loaded. Make sure any objects you want
-	to use are actually available!'''
+	when the function is first loaded.
+	The function will not be called as a daemon; it's (mostly)
+	guaranteed to finish executing (bar interpreter crashes).'''
 	interval = None
 	args = []
 	kwargs = {}
@@ -45,7 +49,7 @@ class ticking(object):
 		self.args = args
 		self.kwargs = kwargs
 	def __call__(self, f):
-		@background
+		@async
 		def wrapper():
 			while True:
 				f(*self.args, **self.kwargs)
